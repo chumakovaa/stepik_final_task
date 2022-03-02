a0fb10433e90289d91ee6715967994e3440e6063
Elena Chumakova <chumakowa.elena2016@yandex.ru>
2022-03-02 12:53:13 +0800

    Added solve_quiz_and_get_code method

--- pages/base_page.py
+++ pages/base_page.py
@@ -16,3 +16,18 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+
+    def solve_quiz_and_get_code(self):
+        from math import log, sin
+        alert = self.browser.switch_to.alert
+        x = alert.text.split()[2]
+        answer = str(log(abs((12 * sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")

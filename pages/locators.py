c45ea098fac861740b8d2794821f3f2d77aafdc2
Elena Chumakova <chumakowa.elena2016@yandex.ru>
2022-03-02 12:52:55 +0800

    Added productPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,7 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, 'login_form')
     REGISTER_FORM = (By.ID, 'register_form')
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, 'btn-add-to-basket')

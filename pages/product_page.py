c987c10fafe9fe0a88efd4f5e9d9a0c37a51359a
Elena Chumakova <chumakowa.elena2016@yandex.ru>
2022-03-02 19:33:18 +0800

    Updated product_page.compare_book_price, product_page.compare_book_name methods

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,6 +1,5 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
-from selenium.common.exceptions import NoAlertPresentException
 
 
 class ProductPage(BasePage):
@@ -8,14 +7,14 @@ class ProductPage(BasePage):
         add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_to_basket_button.click()
 
-    def compare_book_name(self):
+    def compare_book_price(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
-        product_price_text = product_price.text.split('&')[0]
+        product_price_text = product_price.text
         message_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE_MESSAGE)
         message_price_text = message_price.text
         assert product_price_text == message_price_text, f'Should be {product_price_text}'
 
-    def compare_book_price(self):
+    def compare_book_name(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
         product_name_text = product_name.text
         message_name = self.browser.find_element(*ProductPageLocators.NAME_OF_SUCCESSFULLY_ADDED_BOOK)

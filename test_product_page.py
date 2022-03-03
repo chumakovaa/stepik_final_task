fff363dab28fad8dbb178c4c2f6be257309aa141
Elena Chumakova <chumakowa.elena2016@yandex.ru>
2022-03-03 17:50:56 +0800

    Created TestUserAddToBasketFromProductPage class

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,14 @@
 from .pages.product_page import ProductPage
 import pytest
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
+import faker
 
 link_of_the_product = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+login_link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('offer_number', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail),
                                           "8", "9"])
 def test_guest_can_add_product_to_basket(browser, offer_number):
@@ -45,13 +49,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link_of_the_product)
     page.open()
     page.can_go_to_login_page
 
 
-@pytest.mark.new
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, link_of_the_product)
     page.open()
@@ -59,3 +64,30 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
     basket_page.should_be_basket_empty_message()
+
+
+@pytest.mark.new
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope='function', autouse=True)
+    def setup(self, browser):
+        f = faker.Faker()
+        email = f.email()
+        password = f.company()
+        login_page = LoginPage(browser, login_link)
+        login_page.open()
+        login_page.register_new_user(email, password)
+        login_page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        product_page = ProductPage(browser, link_of_the_product)
+        product_page.open()
+        product_page.should_not_be_success_message()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        product_page = ProductPage(browser, link_of_the_product)
+        product_page.open()
+        product_page.add_to_basket()
+        product_page.compare_book_price()
+        product_page.compare_book_name()
+

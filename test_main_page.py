bfe7dac746c8a1a6e342eb1d91deb38b2a7951bd
Elena Chumakova <chumakowa.elena2016@yandex.ru>
2022-03-03 13:37:04 +0800

    Edited test_guest_cant_see_product_in_basket_opened_from_product_page

--- test_main_page.py
+++ test_main_page.py
@@ -30,3 +30,4 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page.should_be_basket_empty_message()
 
 
+
--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,8 @@
 from .pages.product_page import ProductPage
 import pytest
+from .pages.basket_page import BasketPage
 
-link_of_the_product = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+link_of_the_product = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
 @pytest.mark.parametrize('offer_number', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail),
@@ -38,17 +39,23 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.success_message_should_disappear()
 
 
-@pytest.mark.new
 def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link_of_the_product)
     page.open()
     page.should_be_login_link()
 
 
-@pytest.mark.new
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link_of_the_product)
     page.open()
     page.can_go_to_login_page
 
 
+@pytest.mark.new
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    page = ProductPage(browser, link_of_the_product)
+    page.open()
+    page.can_go_to_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_basket_empty_message()

446d08c13fbc86adcd84d3a166d2adaa04f6f364
Elena Chumakova <chumakowa.elena2016@yandex.ru>
2022-03-03 10:41:06 +0800

    Transported can_go_to_login_page and should_be_login_link methods to base_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, 'login_form')
     REGISTER_FORM = (By.ID, 'register_form')
@@ -15,4 +11,10 @@ class ProductPageLocators():
     PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
     PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
     NAME_OF_SUCCESSFULLY_ADDED_BOOK = (By.CSS_SELECTOR, '.alert-success:first-child strong')
-    BASKET_PRICE_MESSAGE = (By.CSS_SELECTOR, '.alert-info strong')
+    PRICE_MESSAGE_OF_SUCCESSFULLY_ADDED_BOOK = (By.CSS_SELECTOR, '.alert-info strong')
+    SUCCESS_MESSAGE_BOOK_ADDED = (By.CLASS_NAME, 'alertinner')
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")

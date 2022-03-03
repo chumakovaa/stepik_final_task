6166797b11fef85f35f644670fee132fb8723828
Elena Chumakova <chumakowa.elena2016@yandex.ru>
2022-03-03 17:48:25 +0800

    Added register_new_user method

--- pages/login_page.py
+++ pages/login_page.py
@@ -19,3 +19,13 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'Register form is not presented'
+
+    def register_new_user(self, email, password):
+        input_register_email = self.browser.find_element(*LoginPageLocators.INPUT_REGISTER_EMAIL)
+        input_register_email.send_keys(email)
+        input_register_password = self.browser.find_element(*LoginPageLocators.INPUT_REGISTER_PASSWORD)
+        input_register_password.send_keys(password)
+        input_repeat_register_password = self.browser.find_element(*LoginPageLocators.INPUT_REPEAT_REGISTER_PASSWORD)
+        input_repeat_register_password.send_keys(password)
+        submit_registration_button = self.browser.find_element(*LoginPageLocators.SUBMIT_REGISTRATION_BUTTON)
+        submit_registration_button.click()

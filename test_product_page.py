23ca59eed3013d4b82c1fdd3c276282e6e24a8c6
Elena Chumakova <chumakowa.elena2016@yandex.ru>
2022-03-03 10:44:10 +0800

    Added methods for checking success message and for going to login page

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
 from .pages.product_page import ProductPage
 import pytest
 
+link_of_the_product = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+
 
 @pytest.mark.parametrize('offer_number', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail),
                                           "8", "9"])
@@ -12,4 +14,41 @@ def test_guest_can_add_product_to_basket(browser, offer_number):
     product_page.solve_quiz_and_get_code()
     product_page.compare_book_price()
     product_page.compare_book_name()
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    product_page = ProductPage(browser, link_of_the_product)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    product_page = ProductPage(browser, link_of_the_product)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    product_page = ProductPage(browser, link_of_the_product)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.success_message_should_disappear()
+
+
+@pytest.mark.new
+def test_guest_should_see_login_link_on_product_page(browser):
+    page = ProductPage(browser, link_of_the_product)
+    page.open()
+    page.should_be_login_link()
+
+
+@pytest.mark.new
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    page = ProductPage(browser, link_of_the_product)
+    page.open()
+    page.can_go_to_login_page
+
 
